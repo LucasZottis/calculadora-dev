52d77a576322487ebd9a71685b49ad8f042758bf
Lucas Zottis <zottis.lucas@hotmail.com>
2024-04-22 12:40:34 -0300

    Implementado tela de conversão de tempo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from 'src/pages/home-page/home-page.component';
+import { TimeConverterPageComponent } from 'src/pages/time-converter-page/time-converter-page.component';
 
 export const routes: Routes = [
-    {path: '', component: HomePageComponent}
+    {path: '', component: HomePageComponent},
+    {path: 'conversores/conversor-tempo', component: TimeConverterPageComponent}
 ];
