98a88e7ea0ba798d08313a07cbfa993c561e72dc
Lucas Zottis <zottis.lucas@hotmail.com>
2024-04-25 06:45:13 -0300

    Implementado travas para impedir outro digito alem de numero

--- src/pages/validators/cpf-validator-page/cpf-validator-page.component.ts
+++ src/pages/validators/cpf-validator-page/cpf-validator-page.component.ts
@@ -55,24 +55,82 @@ export class CpfValidatorPageComponent {
     return this.validateDigit(this.getDigits(cpf) + this.getFirstDigit(cpf), this.getSecondDigit(cpf), 11);
   }
 
-  private removeMask(cpf: string): string {
-    return cpf.replace(".", "").replace("/", "").replace("-", "");
+  private removeMask(value: string): string {
+    return value.replaceAll(".", "").replace("/", "").replace("-", "");
   }
 
-  public validate(): void {
+  private count(value: string, searchedValue: string): number {
+    let count = 0;
+
+    value.split("").forEach(d => {
+      if (d === searchedValue)
+        count++;
+    });
+
+    return count;
+  }
+
+  validate(): void {
     let formatoValido = (this.cpf !== null || this.cpf !== undefined);
 
     if (!formatoValido)
       return;
 
-    this.cpf = this.removeMask(this.cpf);
+    let cpf = this.removeMask(this.cpf);
 
-    if (this.validateFirstDigit(this.cpf) && this.validateSecondDigit(this.cpf)) {
+    if (this.validateFirstDigit(cpf) && this.validateSecondDigit(cpf)) {
       this.result = "Válido";
-      this.style = "color: green";
+      this.style = "color: var(--primary-color)";
     } else {
       this.result = "Inválido";
       this.style = "color: red";
     }
+  }
+
+  onKeypress(e: KeyboardEvent) {
+    if (Number.isNaN(Number(e.key)))
+      e.preventDefault();
+    else {
+      let cpf = this.removeMask(this.cpf);
+
+      switch (cpf.length) {
+        case 3:
+        case 6:
+          if (this.count(this.cpf, ".") !== 2)
+            this.cpf += ".";
+          break;
+        case 9:
+          if (this.count(this.cpf, ".") !== 1)
+            this.cpf += "-";
+          break;
+      }
+    }
+  }
+
+  onPaste(args: ClipboardEvent) {
+    this.cpf = "";
+    let value = (args.clipboardData?.getData("text") ?? "");
+    for (let index = 0; index < value.length; index++) {
+      if (!Number.isNaN(Number(value[index])) && this.cpf.length < 14) {
+        let cpf = this.removeMask(this.cpf);
+
+        switch (cpf.length) {
+          case 3:
+          case 6:
+            if (this.count(this.cpf, ".") !== 2)
+              this.cpf += ".";
+            break;
+          case 9:
+            if (this.count(this.cpf, ".") !== 1)
+              this.cpf += "-";
+            break;
+        }
+
+        this.cpf += value[index];
+      }
+
+    }
+
+    args.preventDefault();
   }
 }
