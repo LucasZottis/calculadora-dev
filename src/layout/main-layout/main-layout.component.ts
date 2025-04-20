ed0c96ac7d16846d23fb3dcc886d27ec48fad91c
Lucas Zottis <zottis.lucas@hotmail.com>
2025-04-20 17:28:18 -0300

    COmmit

--- src/layout/main-layout/main-layout.component.ts
+++ src/layout/main-layout/main-layout.component.ts
@@ -11,16 +11,16 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 export class MainLayoutComponent implements OnInit {
   ngOnInit(): void {
-    document.addEventListener('DOMContentLoaded', function () {
-      const menuItems = document.querySelectorAll('.menu-item');
-      menuItems.forEach(item => {
-        item.addEventListener('click', function (e) {
-          e.preventDefault();
-          menuItems.forEach(i => i.classList.remove('active'));
-          this.classList.add('active');
-        });
-      });
-    });
+    // document.addEventListener('DOMContentLoaded', function () {
+    //   const menuItems = document.querySelectorAll('.menu-item');
+    //   menuItems.forEach(item => {
+    //     item.addEventListener('click', function (e) {
+    //       e.preventDefault();
+    //       menuItems.forEach(i => i.classList.remove('active'));
+    //       this.classList.add('active');
+    //     });
+    //   });
+    // });
   }
 
 }
