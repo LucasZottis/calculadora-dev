3ec30b4a23403c8c7cccfcd5a71bcb170bd52e5f
Lucas Zottis <zottis.lucas@hotmail.com>
2025-08-31 10:46:01 -0300

    Comentado a tag do clarity pois será visto futuramente a questão dos cookies do clarity

--- src/shared/components/cookie-consent/cookie-consent.component.ts
+++ src/shared/components/cookie-consent/cookie-consent.component.ts
@@ -23,6 +23,16 @@ export class CookieConsentComponent implements OnInit, OnDestroy {
 
   constructor(private cookieService: CookieService) { }
 
+  private setMicrosoftClarityConsent(adStorage: boolean, analyticStorage: boolean): void {
+    if (!window.clarity)
+      return;
+
+    window.clarity('consentv2', {
+      ad_Storage: adStorage ? "granted" : "denied",
+      analytics_Storage: analyticStorage ? "granted" : "denied"
+    });
+  }
+
   ngOnInit(): void {
     // Verifica se o usuário já aceitou os cookies
     const temConsentimento = this.cookieService.verificarConsentimento();
@@ -38,6 +48,7 @@ export class CookieConsentComponent implements OnInit, OnDestroy {
 
     // Adiciona listener para o evento personalizado
     window.addEventListener('mostrar-cookie-dialog', this.abrirModal.bind(this));
+    window.clarity =>
   }
 
   ngOnDestroy(): void {
